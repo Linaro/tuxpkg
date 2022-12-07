Linaro/tuxpkg

commit 6a7c9b3c8e08a7cce8e607cf09fa05e180634fbc

makefile: handle dashes in package names
Assume the module name is the same as the package name but with dashes
(-) replaced with underscores (_).

tuxpkg/data/tuxpkg.mk
@@ -1,9 +1,10 @@
 .PHONY: test
 
+MODULE ?= $(shell echo $(PROJECT) | sed -e 's/-/_/g')
 TUXPKG_MIN_COVERAGE ?= 100
 
 test:
-	python3 -m pytest --cov=$(PROJECT) --cov-report=term-missing --cov-report=xml:coverage.xml --cov-fail-under=$(TUXPKG_MIN_COVERAGE)
+	python3 -m pytest --cov=$(MODULE) --cov-report=term-missing --cov-report=xml:coverage.xml --cov-fail-under=$(TUXPKG_MIN_COVERAGE)
 
 style:
 	black --check --diff .
@@ -14,16 +15,16 @@ flake8:
 typecheck:
 	mypy --exclude=dist/ .
 
-version ?= $(shell sed -e '/^__version__/ !d; s/"\s*$$//; s/.*"//' $(PROJECT)/__init__.py)
+version ?= $(shell sed -e '/^__version__/ !d; s/"\s*$$//; s/.*"//' $(MODULE)/__init__.py)
 
 CLEAN += dist
 
-rpm: dist/$(PROJECT)-$(version)-0$(PROJECT).noarch.rpm
+rpm: dist/$(PROJECT)-$(version)-0$(MODULE).noarch.rpm
 
 RPMBUILD = rpmbuild
-dist/$(PROJECT)-$(version)-0$(PROJECT).noarch.rpm: dist/$(PROJECT)-$(version).tar.gz dist/$(PROJECT).spec
+dist/$(PROJECT)-$(version)-0$(MODULE).noarch.rpm: dist/$(PROJECT)-$(version).tar.gz dist/$(PROJECT).spec
 	cd dist && \
-	$(RPMBUILD) -ta --define "dist $(PROJECT)" --define "_rpmdir $$(pwd)" $(PROJECT)-$(version).tar.gz
+	$(RPMBUILD) -ta --define "dist $(MODULE)" --define "_rpmdir $$(pwd)" $(PROJECT)-$(version).tar.gz
 	mv $(patsubst dist/%, dist/noarch/%, $@) $@
 	rmdir dist/noarch
 

commit b48e00e2bdb5746131bd296ddbae5b1efef8ad50

data: tuxpkg.mk: run: uses wrong module name
The module name in the run script in the rpm package can't find the
correct 'MODULE' name:

'/usr/bin/python3: No module named lava-test-plans'

Use the 'MODULE' variable instead of the 'PROJECT' variable.

Fixes: 6a7c9b3c8e08 ("makefile: handle dashes in package names")
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

tuxpkg/data/tuxpkg.mk
@@ -62,7 +62,7 @@ run:
 	echo "set -eu" >> $@
 	echo 'realfile="$$(readlink -f "$$0")"' >> $@
 	echo 'export PYTHONPATH="$$(dirname "$$realfile")"' >> $@
-	echo 'exec python3 -m $(PROJECT) "$$@"' >> $@
+	echo 'exec python3 -m $(MODULE) "$$@"' >> $@
 	chmod +x run
 
 release: